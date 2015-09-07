e37582e71246618482c6f07ad5683fdbd43eac08
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-06 22:03:52 -0700

    Build footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def include_pertinent_events
-    @next_event = Event.after.by_date.first
-    @prev_event = Event.before.by_date.last
+    @future_events = Event.after.by_date.limit(4)
+    @past_events = Event.before.by_date.limit(4)
+    @next_event = @future_events.first
+    @prev_event = @past_events.last
+
     # An event in the past 2 days is still recent enough to consider "primary"
     prev_date = @prev_event.try(:date) || Date.new(2010, 1, 1)
     is_recent = Date.today - prev_date > 2.days
