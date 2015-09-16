88c08b7ea6edc234740832e1d512cb21d9b63d29
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-16 16:52:26 -0700

    Update SponsorshipTier model

--- app/models/sponsorship_tier.rb
+++ app/models/sponsorship_tier.rb
@@ -13,7 +13,23 @@
 #
 
 class SponsorshipTier < ActiveRecord::Base
-  belongs_to :event
+  belongs_to :event, touch: true
   has_many :sponsorships
   has_many :companies, through: :sponsorships
+  serialize :details
+
+  validates :name, presence: true
+  validates :event, presence: true
+  validates :color, presence: true, length: { maximum: 7 }
+
+  scope :available, ->{ where.not(price: nil) }
+  scope :by_price, ->{ order('price DESC') }
+
+  def available?
+    !unavailable?
+  end
+
+  def unavailable?
+    price.nil?
+  end
 end
