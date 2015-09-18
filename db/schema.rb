9a6bc816e6881edfe223729a30e9c081b85e79db
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-17 18:09:55 -0700

    Add URL column to Company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917193206) do
+ActiveRecord::Schema.define(version: 20150918003218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150917193206) do
     t.string   "contact"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.string   "url"
   end
 
   add_index "companies", ["name"], name: "index_companies_on_name", unique: true, using: :btree
