c3fd4dff5e48d96070ebb4f588870ab4780bd8ac
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-24 16:51:12 -0700

    Use custom sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions',
     passwords: 'users/passwords'
   }
 
