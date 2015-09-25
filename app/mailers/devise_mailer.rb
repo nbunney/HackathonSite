8a609d6ea8b089203344daaf606a4bda035e9e1a
Peter Lejeck <peter.lejeck@gmail.com>
2015-09-24 17:37:38 -0700

    Fix logo in confirmation email

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -6,4 +6,12 @@ class DeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
 
   default template_path: 'devise/mailer'
+
+  def roadie_options
+    opts = super.merge({
+      url_options: Rails.application.config.action_mailer.default_url_options
+    })
+    p opts
+    opts
+  end
 end
