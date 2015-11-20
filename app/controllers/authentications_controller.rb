fb63658f2ac2e046649b89e916598564986915ca
Peter Lejeck <peter.lejeck@gmail.com>
2015-11-20 14:21:44 -0800

    Add authentications controller and change prefix for OmniAuth

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,42 @@
+class AuthenticationsController < ApplicationController
+  # GET /authentications/:provider/callback
+  def create
+    if user_signed_in?
+      # Add to existing account
+      Authentication.create!(
+        service: auth[:provider],
+        uid: auth[:uid],
+        user: current_user
+      )
+      redirect_to :home, notice: "An account at #{auth[:provider]} has been linked to your account"
+    elsif authentication = find_auth
+      sign_in! authentication.user
+    else
+      session[:prefill_signup] = signup_info
+      redirect_to new_user_registration_path
+    end
+  end
+
+  private
+  def find_auth
+    Authentication.where(service: auth[:provider], uid: auth[:uid]).first
+  end
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+  def signup_info
+    {
+      auth: {
+        service: auth[:provider],
+        uid: auth[:uid]
+      },
+      user: {
+        name: auth[:info][:name],
+        email: auth[:info][:email],
+        image: auth[:info][:image]
+      }
+    }
+  end
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  configure do |config|
+    config.path_prefix = '/authentications'
+  end
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
